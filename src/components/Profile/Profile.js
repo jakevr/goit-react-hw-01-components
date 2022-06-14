8dfb01f3a389bfdbadc1762de7691c38be5d7db3
Ievgen Rezvaniuk <rezvaniuk@gmail.com>
2022-06-14 20:31:40 +0300

    Profile Impl

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types';
+import s from './Profile.module.css';
+
+export default function Profile({ username, tag, location, avatar, stats }) {
+  Profile.propTypes = {
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    stats: PropTypes.array.isRequired, // https://www.npmjs.com/package/prop-types
+  };
+
+  return (
+    <div className={s.profile}>
+      <div className={s.description}>
+        <img src={avatar} alt={tag} width={300} className={s.avatar} />
+        <p className={s.name}>{username}</p>
+        <p className={s.tag}>@{tag}</p>
+        <p className={s.location}>{location}</p>
+      </div>
+      <ul>
+        <li>
+          <span className={s.label}>Followers</span>
+          <span className={s.quantity}>{stats.followers}</span>
+        </li>
+        <li>
+          <span className={s.label}>Views</span>
+          <span className={s.quantity}>{stats.views}</span>
+        </li>
+        <li>
+          <span className={s.label}>Likes</span>
+          <span className={s.quantity}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
